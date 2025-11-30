35b769b672b25880ab4318f384c955f91b211ff6
Hicham Benoukaiss <hibenouk@e1r4p12.1337.ma>
2025-11-30 21:39:52 +0100

    init

---
+++ backend/drizzle/0000_zippy_zzzax.sql
@@ -0,0 +1,14 @@
+CREATE TABLE "services" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" text NOT NULL,
+	"price" numeric NOT NULL,
+	"created_at" timestamp DEFAULT now()
+);
+--> statement-breakpoint
+CREATE TABLE "users" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" text NOT NULL,
+	"email" text NOT NULL,
+	"created_at" timestamp DEFAULT now(),
+	CONSTRAINT "users_email_unique" UNIQUE("email")
+);
